6e64b09740f455785f99d43df79ad33559b8a09b
Anastacia <anaanestesia@gmail.com>
2024-03-13 13:39:00 +0000

    first mocked table

--- frontend/static/js/pages/Dashboard.js
+++ frontend/static/js/pages/Dashboard.js
@@ -7,11 +7,51 @@ export default class extends Abstract {
 		this.params = props;
 	}
 
+	async addFuncionality() {
+
+	}
+
 	async getHtml() {
 		return `
 			<h1>
 				${i18next.t('dashboard.title')}
 			</h1>
+			<div class="dashboard">
+				<table class="table table-hover text-center">
+					<thead class="table-secondary">
+						<tr>
+							<th scope="col">Game id</th>
+							<th scope="col">Victory</th>
+							<th scope="col">Defeat</th>
+							<th scope="col">Points</th>
+							<th scope="col">Total Points</th>
+						</tr>
+					</thead>
+					<tbody class="table-group-divider" style="border-top-color: #6c757d">
+						<tr>
+							<th scope="row">1</th>
+							<td>x</td>
+							<td></td>
+							<td>5</td>
+							<td>5</td>
+						</tr>
+						<tr>
+							<th scope="row">2</th>
+							<td></td>
+							<td>x</td>
+							<td>3</td>
+							<td>8</td>
+						</tr>
+						<tr>
+							<th scope="row">3</th>
+							<td></td>
+							<td>x</td>
+							<td>2</td>
+							<td>10</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		`;
 	}
 }
