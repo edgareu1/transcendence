7f3be0b6b470bb602c0deeacde8d3952e4169864
Anastacia <anaanestesia@gmail.com>
2024-03-14 18:08:10 +0000

    including tabs on general dashboard

--- frontend/static/js/pages/GeneralDashboard.js
+++ frontend/static/js/pages/GeneralDashboard.js
@@ -69,7 +69,23 @@ export default class extends Abstract {
 			<h1>
 				${i18next.t('generalDashboard.title')}
 			</h1>
-			<div class="dashboard">${this.generateTable()}</div>
+
+			<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+				<li class="nav-item" role="presentation">
+					<button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">History</button>
+				</li>
+				<li class="nav-item" role="presentation">
+					<button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Ranking</button>
+				</li>
+				<li class="nav-item" role="presentation">
+					<button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Graphs</button>
+				</li>
+			</ul>
+			<div class="tab-content" id="pills-tabContent">
+				<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">${this.generateTable()}</div>
+				<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">Ranking is coming here</div>
+				<div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">Graphs are coming here</div>
+			</div>
 		`;
 	}
 }
